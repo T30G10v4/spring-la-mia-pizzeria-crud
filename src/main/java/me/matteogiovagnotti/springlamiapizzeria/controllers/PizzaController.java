8da589dcd516139212b1796358cddadcbab0691c
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-03-30 15:38:37 +0200

    Add show and search

--- src/main/java/me/matteogiovagnotti/springlamiapizzeria/controllers/PizzaController.java
+++ src/main/java/me/matteogiovagnotti/springlamiapizzeria/controllers/PizzaController.java
@@ -4,12 +4,18 @@ package me.matteogiovagnotti.springlamiapizzeria.controllers;
 import me.matteogiovagnotti.springlamiapizzeria.models.Pizza;
 import me.matteogiovagnotti.springlamiapizzeria.repositories.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -19,11 +25,39 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model){
+    public String index(Model model, @RequestParam(name = "name") Optional<String> keyword){
 
-        List<Pizza> pizzas = pizzaRepository.findAll();
+        List<Pizza> pizzas;
+        if(keyword.isEmpty()) {
+
+           pizzas = pizzaRepository.findAll(Sort.by("name"));
+
+        } else {
+
+            pizzas = pizzaRepository.findByNameContainingIgnoreCase(keyword.get());
+            model.addAttribute("keyword", keyword.get());
+
+        }
+
         model.addAttribute("list", pizzas);
         return "/pizzas/index";
+
+    }
+
+    @GetMapping("/{pizzaId}")
+    public String show(@PathVariable("pizzaId") Integer id, Model model){
+
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isPresent()) {
+
+            model.addAttribute("pizza", result.get());
+            return "/pizzas/show";
+
+        } else {
+
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found.");
+
+        }
 
     }
 
